bc68a74726fdb011188c7a5e59050f04c696f418
alfanoK <alfanokurniawan@gmail.com>
2022-06-11 23:25:28 +0700

    challenge 7 promise

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (mad) => {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    return Promise.all([theaterIXX, theaterVGC]).then((hasil) => {
+      const hasilSetelahMenonton = [...hasil[0], ...hasil[1]]; 
+      return hasilSetelahMenonton.filter((item) => item.hasil === mad).length;
+    }).catch ((err) => {
+      console.log(err); 
+    });
+  }
+
 
 module.exports = {
   promiseOutput,
